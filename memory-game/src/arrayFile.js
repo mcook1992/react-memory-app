b45523b3d7aa9a09ac39fb0fed04513b47f05590
Mercer Cook <mercercook@Mercers-MacBook-Pro.local>
2019-05-27 18:55:31 -0400

    game now knows when I'm losing

--- memory-game/src/arrayFile.js
+++ memory-game/src/arrayFile.js
@@ -1,51 +1,63 @@
 const arrayOfCards = [
   {
     id: 1,
-    cardNumber: 1
+    cardNumber: 1,
+    hasBeenClicked: false
   },
   {
     id: 2,
-    cardNumber: 2
+    cardNumber: 2,
+    hasBeenClicked: false
   },
   {
     id: 3,
-    cardNumber: 3
+    cardNumber: 3,
+    hasBeenClicked: false
   },
   {
     id: 4,
-    cardNumber: 4
+    cardNumber: 4,
+    hasBeenClicked: false
   },
   {
     id: 5,
-    cardNumber: 5
+    cardNumber: 5,
+    hasBeenClicked: false
   },
   {
     id: 6,
-    cardNumber: 6
+    cardNumber: 6,
+    hasBeenClicked: false
   },
   {
     id: 7,
-    cardNumber: 7
+    cardNumber: 7,
+    hasBeenClicked: false
   },
   {
     id: 8,
-    cardNumber: 8
+    cardNumber: 8,
+    hasBeenClicked: false
   },
   {
     id: 9,
-    cardNumber: 9
+    cardNumber: 9,
+    hasBeenClicked: false
   },
   {
     id: 10,
-    cardNumber: 10
+    cardNumber: 10,
+    hasBeenClicked: false
   },
   {
     id: 11,
-    cardNumber: 11
+    cardNumber: 11,
+    hasBeenClicked: false
   },
   {
     id: 12,
-    cardNumber: 12
+    cardNumber: 12,
+    hasBeenClicked: false
   }
 ];
 
--- memory-game/src/components/board.js
+++ memory-game/src/components/board.js
@@ -5,33 +5,89 @@ import arrayOfCards from "../arrayFile";
 
 var repeatClick = false;
 
+function shuffle(array) {
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+// function shuffleCards() {
+//   arrayOfCards.forEach(function(element) {
+//     element.cardNumber = Math.floor(Math.random() * arrayOfCards.length);
+//     console.log(element);
+//   });
+
+//   arrayOfCards.sort(function(a, b) {
+//     return a.cardNumber - b.cardNumber;
+//   });
+// }
+
 class Board extends Component {
   state = {
     score: 0,
-    wins: 0
+    wins: 0,
+    vulnerableIDs: []
   };
 
   currentNumber = this.state.score;
 
   handleSquareClicked = event => {
     event.preventDefault();
-    console.log("Button was clicked");
-    console.log(event.target.id);
+    // console.log("Button was clicked");
+    // console.log(event.target.id);
 
     var currentNodeID = event.target.id;
+    console.log(currentNodeID);
 
     var currentNodeClasses = document.getElementById(currentNodeID).classList;
 
-    console.log(currentNodeClasses);
-
-    currentNodeClasses.forEach(function(e) {
-      console.log(e);
-      e == "clicked" ? (repeatClick = true) : currentNodeClasses.add("clicked");
+    this.state.vulnerableIDs.forEach(element => {
+      if (currentNodeID == element) {
+        console.log("you lose");
+      }
     });
 
-    repeatClick
-      ? console.log("You lost!")
-      : this.setState({ score: this.state.score + 1 });
+    // console.log(currentNodeClasses);
+    arrayOfCards.forEach(element => {
+      console.log(element);
+      if (element.id == currentNodeID) {
+        if (element.hasBeenClicked == true) {
+          //   console.log(element);
+          //   console.log("You lose!");
+        } else {
+          element.hasBeenClicked = true;
+          this.state.vulnerableIDs.push(currentNodeID);
+          this.setState({
+            score: this.state.score + 1
+          });
+          shuffle(arrayOfCards);
+        }
+      }
+    });
+
+    //     currentNodeClasses.forEach(function(e) {
+    //       //   console.log(e);
+    //       e == "clicked" ? (repeatClick = true) : console.log("Nothing to do here");
+    //     });
+
+    //     if (repeatClick) {
+    //       console.log("You lost!");
+    //     } else {
+    //     }
   };
 
   //   id => {
@@ -46,7 +102,7 @@ class Board extends Component {
           {arrayOfCards.map(card => (
             <Card
               number={card.cardNumber}
-              hasBeenClicked={false}
+              hasBeenClicked={card.hasBeenClicked}
               id={card.id}
               onClick={this.handleSquareClicked}
             />
