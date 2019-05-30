88ca539debf580f9659ea3e90fe27f5fc4a13797
Mercer Cook <mercercook@Mercers-MacBook-Pro.local>
2019-05-29 21:58:14 -0400

    added image files

--- memory-game/src/arrayFile.js
+++ memory-game/src/arrayFile.js
@@ -2,62 +2,74 @@ const arrayOfCards = [
   {
     id: 1,
     cardNumber: 1,
-    hasBeenClicked: false
+    hasBeenClicked: false,
+    link: "../assets/Bad-Joke-Eel.jpg"
   },
   {
     id: 2,
     cardNumber: 2,
-    hasBeenClicked: false
+    hasBeenClicked: false,
+    link: "../assets/Business-Cat.jpg
   },
   {
     id: 3,
     cardNumber: 3,
-    hasBeenClicked: false
+    hasBeenClicked: false,
+    link: "../assets/Chemistry-Cat.jpg"
   },
   {
     id: 4,
     cardNumber: 4,
-    hasBeenClicked: false
+    hasBeenClicked: false,
+    link: "../assets/Condescending-Wonka.jpg"
   },
   {
     id: 5,
     cardNumber: 5,
-    hasBeenClicked: false
+    hasBeenClicked: false,
+    link: "../assets/Drunk-Baby.jpg"
   },
   {
     id: 6,
     cardNumber: 6,
-    hasBeenClicked: false
+    hasBeenClicked: false,
+    link: "../assets/First-World-Problems.jpg.webp"
   },
   {
     id: 7,
     cardNumber: 7,
-    hasBeenClicked: false
+    hasBeenClicked: false,
+    link: "../assets/Frye.jpg..webp"
   },
   {
     id: 8,
     cardNumber: 8,
-    hasBeenClicked: false
+    hasBeenClicked: false,
+    link: "../assets/Grumpy-Cat.jpg"
   },
   {
     id: 9,
     cardNumber: 9,
-    hasBeenClicked: false
+    hasBeenClicked: false,
+    link: "../assets/Hipster-Dog.jpg.webp"
   },
   {
     id: 10,
     cardNumber: 10,
-    hasBeenClicked: false
+    hasBeenClicked: false,
+    link: "../assets/Its-the-Children.jpg"
   },
   {
     id: 11,
     cardNumber: 11,
-    hasBeenClicked: false
+    hasBeenClicked: false,
+    link: "../assets/Most-Interesting-Man.jpg"
   },
   {
     id: 12,
     cardNumber: 12,
-    hasBeenClicked: false
+    hasBeenClicked: false,
+    link: "../assets/Most-Interesting-Man.jpg"
   }
 ];
 
